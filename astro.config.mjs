bbe27582e9caf613bfe9c2899374138a8e58f7b5
FloraGabriagues <flora.gabriagues@etik.com>
2026-02-09 12:42:35 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
 export default defineConfig({
-  output: "static",
-  base: "/",
+  site: "https://floragabriagues.github.io",
+  vite: {
+    plugins: [tailwindcss()],
+  }
 });
